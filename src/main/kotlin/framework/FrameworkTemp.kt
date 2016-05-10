ea341994f2b939e9fb45bbe9cdf12b931e1bd8ee
Tal Glanzman <talglanzman@gmail.com>
2016-05-10 23:01:13 +0300

    initial stuff

--- src/main/kotlin/framework/FrameworkTemp.kt
+++
@@ -1,5 +0,0 @@
-package framework
-
-/**
- * Created by talgl_000 on 5/10/2016.
- */
---
+++ src/main/kotlin/runnables/Sanity.kt
@@ -0,0 +1,7 @@
+package runnables
+
+object Sanity {
+    @JvmStatic fun main(args: Array<String>) {
+        println("Running Sanity");
+    }
+}
